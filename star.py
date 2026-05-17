d8405cbf4e0179527729f9d5c2f4af2563b96f8c
Dyan Mel Crisostomo <dmlcrisostomo@gmail.com>
2026-05-17 23:09:12 +0800

    Update star.py
    add function

--- star.py
+++ star.py
@@ -1,1 +1,10 @@
+def draw_pyramid(total_rows, num_star=1):
+    print(" " * (total_rows - num_star) + "* " * num_star)
+
+    if num_star < total_rows:
+        num_star += 1
+        draw_pyramid(total_rows, num_star)
+    else:
+        print(f"Done pyramid with {total_rows} * base")
+
 draw_pyramid(int(input("How many is the base star? ")))
